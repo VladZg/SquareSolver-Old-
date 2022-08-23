7754b35494bdc2d80b5d6d0cfd132417d4387ffa
VladZg <71943700+VladZg@users.noreply.github.com>
2022-08-23 17:56:17 +0300

    Changed type of params (float -> double)

--- SquareSolver.cpp
+++ SquareSolver.cpp
@@ -5,20 +5,21 @@
 #include <math.h>
 #define INF_ROOTS 'infinite number of roots'
 
-int SquareSolver (float a, float b, float c, float* x1, float* x2)
-    {
+int SquareSolver (double a, double b, double c, double* x1, double* x2) {
+
     if (a==0)
         {
-        if (b==0) return INF_ROOTS;
-        else
-            {
+        if (b==0) {
+            return INF_ROOTS;
+        }
+        else {
             *x1 = -c / b;
             return 1;
             }
         }
-    else
-        {
-        float d = b*b - 4*a*c;
+
+    else {
+        double d = b*b - 4*a*c;
 
         if (d > 0)
             {
@@ -37,31 +38,28 @@ int SquareSolver (float a, float b, float c, float* x1, float* x2)
         }
     }
 
-main( )
-    {
+int main( ) {
 
-    float a=0, b=0, c=0, x1, x2;
+    double a=NULL, b=NULL, c=NULL, x1=NULL, x2=NULL;
     int n_roots;
 
     printf ("Type coefficients: ");
-    scanf("%f %f %f", &a, &b, &c);
-    printf("Solving square equation %.3f*x*x + %.3f*x + %.3f = 0\n", a, b, c);
+    scanf("%lf %lf %lf", &a, &b, &c);
+    printf("Solving square equation %lf*x*x + %lf*x + %lf = 0\n", a, b, c);
 
     n_roots = SquareSolver(a, b, c, &x1, &x2);
-    switch(n_roots)
-        {
+    switch(n_roots) {
         case 0: printf("Equation has no roots");
             break;
 
-        case 1: printf("Equation has only 1 root: x = %f", x1);
+        case 1: printf("Equation has only 1 root: x = %lf", x1);
             break;
 
-        case 2: printf("Equation has 2 roots: x1 = %.3f, x2 = %.3f", x1, x2);
+        case 2: printf("Equation has 2 roots: x1 = %.3lf, x2 = %.3lf", x1, x2);
             break;
 
         case INF_ROOTS: printf("Equation has infinite number of roots");
             break;
         }
+    return 0;
     }
-
-
