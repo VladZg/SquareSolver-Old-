09b02b74b3b1c88413fdbcf871f4d27d433cc46f
VladZg <71943700+VladZg@users.noreply.github.com>
2022-08-25 18:46:54 +0300

    Codestyle changes

--- SquareSolverTest.cpp
+++ SquareSolverTest.cpp
@@ -16,26 +16,20 @@ int OneTestSquareSolver (int test_n, double a, double b, double c, int n_r_exp, 
             return 1;
         }
 
-    else
+    else {
         printf("\nTEST #%d: (%lg, %lg, %lg)\n%sFAILED:   n_roots = %d, x1 = %lg, x2 = %lg\n"
                "EXPECTED: n_roots = %d, x1 = %lg, x2 = %lg%s\n", test_n, a, b, c, KRED, n_roots, x1, x2, n_r_exp, x1_exp, x2_exp, KNRM);
 
             return 0;
+        }
     }
 
 void TestSquareSolver () {
 
     int n_right_tests = 0;
     int n_tests = 8;
-
     FILE *file_test_info = fopen("TestInfo.txt", "r");
     double a = 0, b = 0, c = 0, n_r_exp = 0, x1_exp = 0, x2_exp = 0;
-
-    // for (int i = 1; i <= 8; i++) {
-    //     fscanf(file_test_info, "%lg %lg %lg %lg %lg %lg", &a, &b, &c, &n_r_exp, &x1_exp, &x2_exp);
-    //     n_right_tests += OneTestSquareSolver(i, a, b, c, n_r_exp, x1_exp, x2_exp);
-    //     n_tests = i;
-    //     }
 
     int i = 1;
     while (!feof(file_test_info)) {
